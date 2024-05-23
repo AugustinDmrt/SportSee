20c30a14ad6954dab6472cea01c901b6a91d02ae
Augustin Damourette <a.damourette@gauthier.corp>
2024-05-23 15:24:07 +0200

    Finalisation du style barchart

--- SportSee/src/components/DailyActivities/DailyActivities.tsx
+++ SportSee/src/components/DailyActivities/DailyActivities.tsx
@@ -10,29 +10,36 @@ import {
 import DataAverageSession from "../../assets/data/data.tsx";
 import "./DailyActivities.sass";
 
+// Fonction pour formater les dates
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  return `${day}-${month}`;
+};
+
 const DailyActivities = () => {
   const data = DataAverageSession[1][0].sessions;
-  const dataJS = data.map(
-    (session: { [x: string]: any; day: any; sessionLength: any }) => ({
-      name: `Jour ${session.day}`,
-      pv: session.kilogram,
-      uv: session.calories,
-    })
-  );
-  const CustomTooltip = (data: any) => {
-    try {
-      let kg = data.payload[0]["value"];
-      let kCal = data.payload[1]["value"];
+  const dataJS = data.map((session) => ({
+    name: formatDate(session.day), // Formatage de la date
+    pv: session.kilogram,
+    uv: session.calories,
+  }));
 
-      return (
-        <div className="custom-tooltip">
-          <p className="label">{`${kg}kg`}</p>
-          <p className="label">{`${kCal}Kcal`}</p>
-        </div>
-      );
-    } catch {
+  const CustomTooltip = ({ payload }) => {
+    if (!payload || payload.length === 0) {
       return null;
     }
+
+    const kg = payload[0].value;
+    const kCal = payload[1].value;
+
+    return (
+      <div className="custom-tooltip">
+        <p className="label">{`${kg}kg`}</p>
+        <p className="label">{`${kCal}Kcal`}</p>
+      </div>
+    );
   };
 
   return (
@@ -40,8 +47,20 @@ const DailyActivities = () => {
       <h2 className="daily-chart-title">Activité quotidienne</h2>
       <BarChart width={835} height={320} data={dataJS}>
         <CartesianGrid horizontal strokeDasharray="3 3" vertical={false} />
-        <XAxis dataKey="name" />
-        <YAxis yAxisId="right" dataKey="pv" orientation="right" />
+        <XAxis
+          dataKey="name"
+          axisLine={false}
+          tickLine={false}
+          tick={{ fill: "#9B9EAC" }}
+        />
+        <YAxis
+          yAxisId="right"
+          dataKey="pv"
+          orientation="right"
+          axisLine={false}
+          tickLine={false}
+          tick={{ fill: "#9B9EAC" }}
+        />
         <YAxis yAxisId="left" dataKey="uv" orientation="left" hide={true} />
         <Tooltip
           offset={40}
@@ -73,4 +92,5 @@ const DailyActivities = () => {
     </div>
   );
 };
+
 export default DailyActivities;
